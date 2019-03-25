2b53d27a9751543ba0d87bc40e1de8adc930f29d
Ravi Kumar <ravi.kumar@bluereport.net>
2019-03-25 21:46:34 +0100

    Create model Hero

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_03_25_204342) do
+
+  create_table "heros", force: :cascade do |t|
+    t.string "name"
+    t.string "real_name"
+    t.integer "health"
+    t.integer "armour"
+    t.integer "shield"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
