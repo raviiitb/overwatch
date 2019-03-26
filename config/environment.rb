cdaa0e75299b441988dd239e4f6f01c01c581f53
Ravi Kumar <ravi.kumar@bluereport.net>
2019-03-26 09:14:21 +0100

    Add external api endpoint in config

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 # Load the Rails application.
 require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+CONFIG = YAML.load_file("#{Rails.root}/config/common.yml")
