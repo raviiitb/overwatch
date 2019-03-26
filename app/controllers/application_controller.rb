8795294afc9a3905cb85d732f532458bec563ab4
Ravi Kumar <ravi.kumar@bluereport.net>
2019-03-26 09:17:00 +0100

    Desined API  client

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  before_action :client
+
+  private
+
+  def client
+    @client = ApiClient.new(CONFIG['overwatch_api_endpoint'])
+  end
 end
